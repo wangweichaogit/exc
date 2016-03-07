1e67ad5b7e25b63cb3650a6b55c7e42ace430f7b
weichao wang <weichao1990@163.com>
2016-03-07 11:17:25 +0800

    add teststruct

--- teststruct.c
+++ teststruct.c
@@ -6,12 +6,21 @@ struct message
 	int length;
 	char data[0];
 };
+//这两种用法区别是，第一个结构体只用分配一次连续的内存
+//而第二个结构需要分配两次内存，切分配的内存是不连续的
+struct message2
+{
+	int length;
+	char *data;
+};
 
 int main()
 {
 	printf("size = %d\n",sizeof(message));
-	message *mg = (message *)malloc(12);
-	for (int i = 0; i < 8 ; i++)
+	int length = 10;
+	message *mg = (message *)malloc(sizeof(message)+length);
+	mg->length = length;
+	for (int i = 0; i < mg->length ; i++)
 	{
 		mg->data[i] ='1';
 	}
