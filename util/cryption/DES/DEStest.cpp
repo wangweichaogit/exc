7d89a6117967f6bed84fe08d8ebc35f980e5208e
weichao wang <weichao1990@163.com>
2016-03-31 17:37:47 +0800

    delete

--- util/cryption/DES/DEStest.cpp
+++ util/cryption/DES/DEStest.cpp
@@ -4,17 +4,17 @@ using namespace std;
 
 int main(int argc,const char *argv[])
 {
-		if (argc != 2) {
-		cout<<"usage: "<<argv[0]<<" string\n"<<endl;
+		if (argc != 3) {
+		cout<<"usage: "<<argv[0]<<"flags"<<" string\n"<<endl;
 		return -1;
 	}
 	EasyDES mydes(argv[1]);
 	string str,str2;
 	//对ab加密到str
-	mydes.encrypt("ab",str);
+	mydes.encrypt(argv[2],str);
 	//str解密到str2
 	mydes.decrypt(str.c_str(),str2);
-	cout<<str.c_str()<<endl;
+	cout<<argv[1]<<str.c_str()<<endl;
 	cout<<str2.c_str()<<endl;
 	return 0;
 }
