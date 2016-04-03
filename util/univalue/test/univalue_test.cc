f368acdedc54daefece2978be9cccf175a225e52
weichao wang <weichao1990@163.com>
2016-04-03 18:10:37 +0800

    univalue

--- util/univalue/test/univalue_test.cc
+++ util/univalue/test/univalue_test.cc
@@ -1,7 +1,7 @@
 #include "../univalue.h"
 #include <iostream>
 #include <vector>
-
+//转换为字符串输出 使用write(2) 方法 转换为varray
 void printUniValue(UniValue uv)
 {
 	std::string strPrint;
@@ -11,9 +11,7 @@ void printUniValue(UniValue uv)
 		strPrint = uv.get_str();
 	else
 		strPrint = uv.write(2);
-		std::cout<<"//////////////"<<std::endl;
 	std::cout<<strPrint.c_str()<<std::endl;
-		std::cout<<"\ \\\\\\\\\\\\"<<std::endl;
 }
 int main()
 {
@@ -27,15 +25,33 @@ int main()
 	uv.pushKV("uv",true);
 	
 	UniValue uv2;
-	uv2.push_back(uv);
+	if (uv2.push_back(uv))
+			std::cout<<uv2.type()<<std::endl;
 	printUniValue(uv);
 	printUniValue(uv2);	
 	
 	////////////////////////////// string
-	UniValue uv3(UniValue::VSTR);
+	UniValue uv3(UniValue::VARR);
 	uv3.push_back("test string");
-	uv3.push_back("test string 2");
-	printUniValue(uv3);
+		uv3.push_back("test string 2");
+			std::cout<<uv3.type()<<std::endl;
+	std::cout<<uv3.write(2).c_str()<<std::endl;
 
+	/////////////////////////////    ARRAY
+	
+	UniValue uv4(UniValue::VARR);
+	uv4.push_back(uv);
+		uv4.push_back(uv2);
+		uv4.push_back(uv3);
+		
+		for (int i = 0; i < uv4.size() ; i++) {
+			printUniValue(uv4[i]);
+	}
+		#if 0
+	////////////////////////////////////
+		UniValue uv4(UniValue::VNUM);
+		uv4.push_back(10);
+		std::cout<<uv4.getValStr().c_str()<<std::endl;
+	#endif
 	return 0;
 }
